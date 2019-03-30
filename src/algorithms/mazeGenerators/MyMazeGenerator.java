6515bbce043c3f3fcf6fa27e5bd196e37c8ae7a0
Tali <talisch@post.bgu.ac.il>
2019-03-30 21:06:45 +0200

    ashkara mazeeee

--- src/algorithms/mazeGenerators/MyMazeGenerator.java
+++ src/algorithms/mazeGenerators/MyMazeGenerator.java
@@ -1,11 +1,65 @@
 package algorithms.mazeGenerators;
 
 import java.util.Stack;
+import java.util.LinkedList;
+import java.util.Random;
+
 
 public class MyMazeGenerator extends AMazeGenerator {
 
+    public static final char PASSAGE_CHAR = ' ';
+    public static final char WALL_CHAR = '▓';
+    public static final int WALL = 1;
+    public static final int PASSAGE = 0;
+
+    //private final int maze[][];
+    //private final int col;
+    //private final int row;
+
     @Override
     public Maze generate(int row, int col) {
+
+        Maze maze = new Maze(row, col);
+
+        final LinkedList<int[]> neighbors = new LinkedList<>();
+        final Random random = new Random();
+        int x = random.nextInt(col);
+        int y = random.nextInt(row);
+        neighbors.add(new int[]{x, y, x, y});
+        maze.myMaze= new int[row][col];
+        maze.setAllMaze(row,col,1); //init all maze with value 1
+
+       // maze.initPositions(maze.positions);
+       // Position current = new Position(x, y);
+      //  maze.setMyMaze(current.getRowIndex(),current.getColumnIndex(),0);
+       // current.setVisited(true); //step 1
+
+        while (!neighbors.isEmpty()) {
+            final int[] f = neighbors.remove(random.nextInt(neighbors.size()));
+            x = f[2];
+            y = f[3];
+
+       // maze.myMaze[x][y] = 0;
+
+            if (maze.myMaze[x][y] == WALL) {
+                maze.myMaze[f[0]][f[1]] = PASSAGE;
+                maze.myMaze[x][y] = PASSAGE;
+                if (x >= 2 && maze.myMaze[x - 2][y] == WALL)
+                    neighbors.add(new int[]{x - 1, y, x - 2, y});
+                if (y >= 2 && maze.myMaze[x][y - 2] == WALL)
+                    neighbors.add(new int[]{x, y - 1, x, y - 2});
+                if (x < col - 2 && maze.myMaze[x + 2][y] == WALL)
+                    neighbors.add(new int[]{x + 1, y, x + 2, y});
+                if (y < row - 2 && maze.myMaze[x][y + 2] == WALL)
+                    neighbors.add(new int[]{x, y + 1, x, y + 2});
+            }
+        }
+
+        return maze;
+    }
+}
+
+        /*
         Maze maze = new Maze(row, col);
         //Position positions[][] = new Position[row][col];
         maze.initPositions(maze.positions);
@@ -33,7 +87,6 @@ public class MyMazeGenerator extends AMazeGenerator {
                 current.setVisited(true);
             }
         }
-        return maze;
-    }
+        */
 
-}
+
