7a8393a5d7b68f81d0fdf37151047c8d44dc470d
yszoke <szoke@post.bgu.ac.il>
2019-03-31 12:12:23 +0300

    maze2
    (cherry picked from commit a111bbb2d7783226ecc27219fe613481a7d61d64)
    
    # Conflicts:
    #	src/algorithms/mazeGenerators/MyMazeGenerator.java

--- src/algorithms/mazeGenerators/MyMazeGenerator.java
+++ src/algorithms/mazeGenerators/MyMazeGenerator.java
@@ -16,8 +16,6 @@ public class MyMazeGenerator extends AMazeGenerator {
     //private final int col;
     //private final int row;
 
-
-
     @Override
     public Maze generate(int row, int col) {
 
